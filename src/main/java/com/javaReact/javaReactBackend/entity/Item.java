b135451b7cc335fde013d48d0d9c462b85e5e279
styopa-91 <styopaxxl@gmail.com>
2024-02-05 15:07:17 +0200

    add example of validation

--- src/main/java/com/javaReact/javaReactBackend/entity/Item.java
+++ src/main/java/com/javaReact/javaReactBackend/entity/Item.java
@@ -1,6 +1,7 @@
 package com.javaReact.javaReactBackend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
     private String description;
